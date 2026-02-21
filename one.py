279393252a31327a5e26846df3dfc6a3ab139ecd
Faisal Hayat <168349740+FaisalHayat1122@users.noreply.github.com>
2026-02-21 09:54:20 +0500

    Add files via upload

---
+++ one.py
@@ -0,0 +1,15 @@
+# #Functions in Python
+# def Faisal():
+#      return "Function called!"
+#      
+#      
+# print(Faisal())
+#  
+
+def Faisal():
+    print("This is Function in Python")
+    
+    
+Faisal()
+
+
